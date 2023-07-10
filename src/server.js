becf8bb150babf2f1539427e47a29383b30709eb
Wagner Costa <wagcostvez@gmail.com>
2023-07-10 17:04:12 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 
 const express = require("express");
 const AppError = require("./utils/AppError")
@@ -34,5 +35,5 @@ app.use((error, request, response, next) => {
 });
 
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
