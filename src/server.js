e39b33102f8fd8bd7d5d082dac55c599beee0631
Wagner Costa <wagcostvez@gmail.com>
2024-03-20 21:47:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,14 +8,15 @@ const route = require("./routes");
 const uploadConfig = require("./configs/upload");
 const app = express();
 const cors = require("cors");
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
     });
+
+    next();
+});
 app.use(express.json());
 app.use(cors());
 
